c520d5a14e13ba3989a8ac157264b4db187303da
d_nekh <dnekh.dev@gmail.com>
2023-08-08 20:53:45 +0900

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Elements el1 = new Elements();
+        Elements el2 = new Elements();
+
+        el1.start();
+        el2.start();
+    }
+}
