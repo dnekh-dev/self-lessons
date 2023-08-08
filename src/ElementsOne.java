e2e91f908d92299b1d6c9945bdf095c49ea90923
d_nekh <dnekh.dev@gmail.com>
2023-08-08 21:06:48 +0900

    Second commit

---
+++ src/ElementsOne.java
@@ -0,0 +1,8 @@
+public class ElementsOne extends Thread{
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("El1-2 " + i);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Elements el1 = new Elements();
-        Elements el2 = new Elements();
+        ElementsOne el1 = new ElementsOne();
+        ElementsOne el2 = new ElementsOne();
 
         el1.start();
         el2.start();
+
+        Thread el3 = new Thread(new ElementsTwo());
+        Thread el4 = new Thread(new ElementsTwo());
+        Thread el5 = new Thread((new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 11; i++) {
+                    System.out.println("El5 " + i);
+                }
+            }
+        }));
+
+        el3.start();
+        el4.start();
+        el5.start();
     }
 }
